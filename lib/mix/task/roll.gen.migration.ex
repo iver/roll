127dfe2e52a4bd90c26adbb906c9fc3135f92da9
Iván Jaimes <ivan@iver.mx>
2020-04-10 16:48:43 -0500

    Update migration version

--- lib/mix/task/roll.gen.migration.ex
+++ lib/mix/task/roll.gen.migration.ex
@@ -25,12 +25,12 @@ defmodule Mix.Task.Roll.Gen.Migration do
   The repository must be set under `:ecto_repos` in the
   current app configuration or given via the `-r` option.
   ## Examples
-      mix roll.gen.migration add_posts_table
-      mix roll.gen.migration add_posts_table -r Custom.Repo
+      mix ecto.gen.migration add_posts_table
+      mix ecto.gen.migration add_posts_table -r Custom.Repo
   The generated migration filename will be prefixed with the current
   timestamp in UTC which is used for versioning and ordering.
   By default, the migration will be generated to the
-  "priv/YOUR_REPO/migrations" directory of the current application
+  "priv/YOUR_REPO/roll" directory of the current application
   but it can be configured to be any subdirectory of `priv` by
   specifying the `:priv` key under the repository configuration.
   This generator will automatically open the generated file if
