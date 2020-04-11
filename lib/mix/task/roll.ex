37b308228c56cdd0e6c551321cf8754a985b846e
Iván Jaimes <ivan@iver.mx>
2020-04-10 19:13:27 -0500

    Add task menu

---
+++ lib/mix/task/roll.ex
@@ -0,0 +1,28 @@
+defmodule Mix.Task.Roll do
+  use Mix.Task
+
+  @shortdoc "Prints Roll help information"
+
+  @moduledoc """
+  Prints Roll tasks and their information.
+  mix roll
+  """
+
+  @doc false
+  def run(args) do
+    {_opts, args} = OptionParser.parse!(args, strict: [])
+
+    case args do
+      [] -> general()
+      _ -> Mix.raise("Invalid arguments, expected: mix roll")
+    end
+  end
+
+  defp general() do
+    Application.ensure_all_started(:ecto)
+    Mix.shell().info("Roll v#{Application.spec(:ecto, :vsn)}")
+    Mix.shell().info("A Ecto complement for custom migration for Elixir.")
+    Mix.shell().info("\nAvailable tasks:\n")
+    Mix.Tasks.Help.run(["--search", "roll."])
+  end
+end
